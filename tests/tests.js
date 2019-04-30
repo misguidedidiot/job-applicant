d4d81a6f685acb841dd465ed9625de3ba7b10b81
Wes Griffin <themisguidedidiot@gmail.com>
2019-04-29 23:42:14 -0700

    switched file to get-applicant js file and imported export required js files

--- tests/tests.js
+++ tests/tests.js
@@ -1,20 +1,5 @@
 const test = QUnit.test;
-
-function getApplicant(formData) {
-    const haveFeet = formData.get('have-feet') === 'yes';
-    const sweetFeet = parseInt(formData.get('sweet-feet'));
-
-    const applicant = {
-        name: formData.get('name'),
-        haveFeet: haveFeet,
-        destination: formData.get('destination'),
-        season: formData.getAll('season'),
-        sweetFeet: sweetFeet,
-
-    };
-    return applicant;
-
-}
+import getApplicant from '../src/get-applicant.js';
 
 test('return completed form object', function(assert) {
     //Arrange
