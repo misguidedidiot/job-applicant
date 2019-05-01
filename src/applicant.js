139117391a1f7b8ac5ef3d69df8fe7a6a1e2e487
Wes Griffin <themisguidedidiot@gmail.com>
2019-04-30 22:15:57 -0700

    finished setting up applicant.html and applicant.js and made sure all links were correct.

---
+++ src/applicant.js
@@ -0,0 +1,23 @@
+import applicantApi from './applicant-api.js';
+
+// reference all the DOM elements we need to update
+const name = document.getElementById('name');
+const haveFeet = document.getElementById('have-feet');
+const destination = document.getElementById('destination');
+const season = document.getElementById('season');
+const sweetFeet = document.getElementById('sweetfeet');
+
+// get our applicant from the api
+const applicant = applicantApi.get();
+
+// no applicant? no can do, head back to home page...
+if(!applicant) {
+    window.location = './';
+}
+
+// mediate data into the elements
+name.textContent = applicant.name;
+haveFeet.textContent = applicant.haveFeet;
+destination.textContent = applicant.destination;
+season.textContent = applicant.season;
+sweetFeet.textContent = applicant.sweetFeet;
