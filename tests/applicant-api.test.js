08fe5ef23110ba60779d6fc1271e3fe19584e8a8
Wes Griffin <themisguidedidiot@gmail.com>
2019-05-01 17:44:01 -0700

    cleaned up test file...also forgot to branch, so lat commit was on dev2.

--- tests/applicant-api.test.js
+++ tests/applicant-api.test.js
@@ -4,17 +4,11 @@ const test = QUnit.test;
 QUnit.module('applicant-api');
 
 test('round trip application', (assert) => {
-    //Arrange
-    // Set up your parameters and expectations
     const applicant = { name: 'tester' };
- 
 
-    //Act 
-    // Call the function you're testing and set the result to a const
     applicantApi.save(applicant);
     const result = applicantApi.get();
     
-    //Assert
     assert.deepEqual(result, applicant);
 });
 
